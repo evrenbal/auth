a37744b17b35f2fe0bcd873077731e70c6196555
kaioken <max@mctekk.com>
2017-02-23 06:14:57 -0400

    fix session keys

--- src/models/SessionKeys.php
+++ src/models/SessionKeys.php
@@ -2,6 +2,25 @@
 
 namespace Baka\Auth\Models;
 
-class SessionKeys extends \Baka\Auth\Models\SessionKeys
+class SessionKeys extends \Phalcon\Mvc\Model
 {
+    /**
+     * @var string
+     */
+    public $session_id;
+
+    /**
+     * @var integer
+     */
+    public $user_id;
+
+    /**
+     * @var string
+     */
+    public $last_ip;
+
+    /**
+     * @var string
+     */
+    public $last_login;
 }
